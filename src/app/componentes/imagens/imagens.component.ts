a07bcb48435455595f0886e8fd60c16e5eb1e078
flavio luna ferreira <flaviolunaferreira@gmail.com>
2024-10-18 16:58:24 -0300

    escrevendo o componente imagem

--- src/app/componentes/imagens/imagens.component.ts
+++ src/app/componentes/imagens/imagens.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-imagens',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './imagens.component.html',
   styleUrl: './imagens.component.scss'
 })
 export class ImagensComponent {
 
+  imagens: string[] = [
+    'assets/moveis/img1.jpg',
+    'assets/moveis/img2.jpg',
+    'assets/moveis/img3.jpg',
+    'assets/moveis/img4.jpg'
+  ];
+
+  imagemSelecionada: string = this.imagens[0];
+
+  selecionarImagem(imagem: string) {
+    this.imagemSelecionada = imagem;
+  }
 }
