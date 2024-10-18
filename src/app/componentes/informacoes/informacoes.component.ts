af4f1a8e07da161556b297b83c2064806334b0a1
flavio luna ferreira <flaviolunaferreira@gmail.com>
2024-10-18 18:41:23 -0300

    salvando descricao do produto sem apcao de carrinho

--- src/app/componentes/informacoes/informacoes.component.ts
+++ src/app/componentes/informacoes/informacoes.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-informacoes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './informacoes.component.html',
   styleUrl: './informacoes.component.scss'
 })
 export class InformacoesComponent {
+
+  produto = {
+    nome: 'Sofá de Madeira',
+    preco: '3000',
+    descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut mi sed nisl mollis consequat non nec eros. Aenean cursus, purus in blandit lacinia, erat mi rutrum nisi, at condimentum elit nisl a nunc',
+    cores: ['#FF0000', '#0000FF', '#008000', '#000000'],
+    tamanhos: ['P', 'M', 'G']
+  };
+
 
 }
